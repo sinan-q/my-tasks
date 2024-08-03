327046d2b93e28be994977f3b45ba16b9bd69be2
Sinan Muhammed <sinan9539391118@gmail.com>
2024-08-03 23:29:43 +0530

    Added TaskCard

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     const navigate = useNavigate();
     const logout = useLogout();
     const axiosPrivate = useAxiosPrivate();
-    const [data, setData] = useState("")
+    const [tasks, setTasks] = useState("")
 
     const signOut = async () => {
         await logout();
@@ -16,13 +16,13 @@ const Home = () => {
     }
     const getData = async () => {
         try {
-            const response = await axiosPrivate.get('/data')
-            setData(response.data.data)
-            toast(response.data.message)
+            const response = await axiosPrivate.get('/task')
+            setTasks(response.data.data)
+            //toast(response.data.message)
 
         } catch (err) {
             console.error(err);
-        }
+        }   
     }
     
     const sendData = async () => {
@@ -45,12 +45,25 @@ const Home = () => {
     return (
         <div className="absolute    ">
             <div className='text-3xl text-center'>Template</div>
-            {data}
+            { tasks && tasks.map(task => {
+                return <TaskCard 
+                    task={task}
+                />
+            })}
             <div className="mt-1   ">
                 <button className=" border p-2 m-auto text-center w-full hover:bg-red-400 hover:text-white "onClick={signOut}>Sign Out</button>
             </div>
         </div>
     )
 }
+
+const TaskCard = ({task}) => {
+  return (
+    <div className="border border-black">
+        <div>{task.name}</div>
+    </div>
+  )
+}
+
 
 export default Home
